7805950b1da51141afb5b58cb1e89ab9bec1474b
Christoph Viebig <git@christoph-viebig.de>
2015-09-03 12:29:17 +0200

    Setup and build scripts

---
+++ build.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+dir=$(pwd)
+cd ../arch-base
+docker build -t cviebig/arch-base:latest .
+cd ../arch-docker
+docker build -t cviebig/arch-docker:latest .
+cd ../scratch
+docker build -t cviebig/scratch:latest .
+cd ../arch-jenkins
+docker build -t cviebig/arch-jenkins:latest .
+cd ../arch-build
+docker build -t cviebig/arch-build:latest .
+cd ../arch-build-ocl
+docker build -t cviebig/arch-build-ocl:latest .
+cd ../arch-build-ocl-intel
+docker build -t cviebig/arch-build-ocl-intel:latest .
+cd ../arch-build-ocl-amd
+docker build -t cviebig/arch-build-ocl-amd:latest .
+cd $dir
---
+++ setup.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+cd ..
+git clone https://github.com/cviebig/docker-arch-base.git arch-base
+git clone https://github.com/cviebig/docker-arch-docker.git arch-docker
+git clone https://github.com/cviebig/docker-scratch.git scratch
+git clone https://github.com/cviebig/docker-arch-jenkins.git arch-jenkins
+git clone https://github.com/cviebig/docker-arch-build.git arch-build
+git clone https://github.com/cviebig/docker-arch-build-ocl.git arch-build-ocl
+git clone https://github.com/cviebig/docker-arch-build-ocl-amd.git arch-build-ocl-amd
+git clone https://github.com/cviebig/docker-arch-build-ocl-catalyst.git arch-build-ocl-catalyst
+git clone https://github.com/cviebig/docker-arch-build-ocl-intel.git arch-build-ocl-intel
+git clone https://github.com/cviebig/docker-arch-build-ocl-nvidia.git arch-build-ocl-nvidia
