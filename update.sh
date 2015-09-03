2652d229a557e8db645f91e2f5fe62ca98701544
Christoph Viebig <git@christoph-viebig.de>
2015-09-03 17:26:14 +0200

    Provide update script

---
+++ update.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+dir=$(pwd)
+git pull
+cd ../arch-base
+git pull
+cd ../arch-docker
+git pull
+cd ../scratch
+git pull
+cd ../arch-jenkins
+git pull
+cd ../arch-build
+git pull
+cd ../arch-build-ocl
+git pull
+cd ../arch-build-ocl-intel
+git pull
+cd ../arch-build-ocl-amd
+git pull
+cd $dir
