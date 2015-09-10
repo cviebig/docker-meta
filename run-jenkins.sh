9721e9334a372c8fc65a831cf743b705c81f8694
Christoph Viebig <git@christoph-viebig.de>
2015-09-10 14:16:51 +0200

    Bind Jenkins web interface to loopback device

--- run-jenkins.sh
+++ run-jenkins.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker run --privileged -v $(pwd)/../config-ssh:/root/config-ssh cviebig/arch-jenkins
+docker run --privileged -v $(pwd)/../config-ssh:/root/config-ssh -p 127.0.0.1:8080:8080 cviebig/arch-jenkins
